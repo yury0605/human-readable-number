75e9102b5f4cd84173925ab54d26fed8e6d87b2f
Yury <sendtopepper@gmail.com>
2023-02-02 12:07:54 +0300

    done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function toReadable (number) {
-  
+ 
+function toReadable(number) {
+ var converter = require('number-to-words');
+return converter.toWords(number);
 }
+console.log(toReadable(15))
+
+
+
+
+
