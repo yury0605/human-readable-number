8245153f1e9de4f88499df3ecc6110ebe3cb2dc0
Yury <sendtopepper@gmail.com>
2023-02-02 12:10:16 +0300

    issue done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
  
-function toReadable(number) {
+module.exports = function toReadable(number) {
  var converter = require('number-to-words');
 return converter.toWords(number);
 }
-console.log(toReadable(15))
+
 
 
 
